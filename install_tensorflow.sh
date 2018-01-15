da2ede183cef9026089090c2f4eea3743cf4dc4e
Gustav Louw <yellingintothefan@gmail.com>
2018-01-15 13:38:47 -0800

    fixed tensor flow install

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -5,7 +5,7 @@ TF_TYPE="cpu"
 OS="linux"
 
 # Binary extraction folder.
-TARGET_DIRECTORY="/usr/local"
+TARGET_DIRECTORY="/usr"
 
 # Download binary and install.
 curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.4.1.tar.gz" | tar -C $TARGET_DIRECTORY -xz
